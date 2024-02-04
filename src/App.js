34d2afe9390d98ccf421d99e64f2f0041fe7a509
FaithOgendi <faithbrianna01@gmail.com>
2024-02-04 03:19:32 +0300

    transaction component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, {useState,useEffect} from "react";
 
 
-
 function App() {
-  return (
+  const [transactions, setTransactions] = useState([]);
 
-  );
+
+  useEffect(()=>{
+    fetchTransactions();
+  }, []);
+
+  const fetchTransactions = () => {
+    fetch("http://localhost:3000/transactions")
+      .then((response) => response.json())
+      .then((data) => {
+        setTransactions(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching transactions", error);
+      });
+  };
+
+  return(
+    <div>
+      <h1>BANK OF FLATIRON</h1>
+    </div>
+  )
 }
 
 export default App;
